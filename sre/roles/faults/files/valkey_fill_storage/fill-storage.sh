e6a3a4063883d97a7c43e88d0b7ad290a67e9d37
Gerard Vanloo <gerard.vanloo@ibm.com>
2025-12-10 22:04:18 -0500

    fix: ensure valkey fill storage fault persists (#455)
    ---------
    
    Signed-off-by: Gerard Vanloo <gerard.vanloo@ibm.com>
    Signed-off-by: Rohan R. Arora <rohan.arora@ibm.com>
    Co-authored-by: Rohan R. Arora <rohan.arora@ibm.com>

---
+++ sre/roles/faults/files/valkey_fill_storage/fill-storage.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eou pipefail
+
+RANGE_MAX=10000
+RANGE_MIN=1
+
+fill_storage() {
+    sleep 20 # Delay for a few seconds to allow server to start
+
+    while true; do # Fill the storage with 1M key value pairings
+        key_id=$((RANDOM % (RANGE_MAX - RANGE_MIN + 1) + RANGE_MIN))
+
+        head -c 1m /dev/urandom | valkey-cli -x SET largekey$key_id
+        sleep 1
+    done
+}
+
+(
+    fill_storage
+)& # Run the storage fill task in the background
+
+valkey-server # Start the valkey server
